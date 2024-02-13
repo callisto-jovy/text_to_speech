a36c3839f36d37e2cac49d0f1b73b7629e2390c8
callisto-jovy <48448458+callisto-jovy@users.noreply.github.com>
2024-02-13 16:04:09 +0100

    Refactor the TTS providers previous code

--- src/main/java/de/yugata/tts/provider/StreamElementsTTS.java
+++ src/main/java/de/yugata/tts/provider/StreamElementsTTS.java
@@ -3,8 +3,10 @@ package de.yugata.tts.provider;
 
 import de.yugata.tts.configuration.StreamElementsConfiguration;
 import de.yugata.tts.util.ArrayUtil;
+import de.yugata.tts.util.StringUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -14,58 +16,79 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class StreamElementsTTS extends AbstractTTSProvider {
 
+    /**
+     * API endpoint to the StreamElements text-to-speech engine.
+     */
     private static final String API_ENDPOINT = "https://api.streamelements.com/kappa/v2/speech?";
 
-    private final StreamElementsConfiguration.StreamElementsVoice voice;
+    private final String voice;
 
     public StreamElementsTTS(StreamElementsConfiguration configuration) {
         super(configuration);
-        this.voice = configuration.voice();
+        this.voice = getVoice(configuration.voice());
     }
 
     @Override
-    public File generateTTS(String content) {
-        final String voice = getVoice();
+    public Optional<File> generateTTS(String content) {
 
         try {
-            return generateTTS(content, voice);
+            // Stream elements has a character limit of 3000 chars (see the README of https://github.com/chrisjp/tts)
+
+            final File tempFile = File.createTempFile("tiktoktts", ".mp3", configuration.ttsDirectory());
+            final FileOutputStream fos = new FileOutputStream(tempFile, true);
+
+            final String[] blocks = StringUtil.splitSentences(content, 3000);
+
+            for (final String block : blocks) {
+                generateTTS(block, fos);
+            }
+
+            fos.close();
+
+            return Optional.of(tempFile);
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            LOGGER.error("StreamElements TTS generation failed with an exception of ", e);
+            return Optional.empty();
         }
     }
 
-    private File generateTTS(final String text, final String voice) throws IOException, InterruptedException {
-        final String params = String.format("voice=%s&text=%s", URLEncoder.encode(voice, StandardCharsets.UTF_8), URLEncoder.encode(text, StandardCharsets.UTF_8));
+    private void generateTTS(final String text, final FileOutputStream fileOutputStream) throws IOException, InterruptedException {
+        final String params = String.format("voice=%s&text=%s",
+                URLEncoder.encode(voice, StandardCharsets.UTF_8),
+                URLEncoder.encode(text, StandardCharsets.UTF_8)
+        );
 
 
         final HttpClient client = HttpClient.newHttpClient();
         final HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(API_ENDPOINT.concat(params)))
+                .uri(URI.create(API_ENDPOINT + params))
                 .setHeader("User-Agent", USER_AGENT)
                 .GET()
                 .build();
 
         final HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
-        final File tempFile = File.createTempFile("streamlabstts", ".mp3");
 
-        try (final InputStream body = response.body()) {
-
-            if (response.statusCode() == 200) {
-                body.transferTo(Files.newOutputStream(tempFile.toPath()));
-            } else {
-                body.transferTo(System.out);
-                throw new RuntimeException("Illegal response code: " + response.statusCode());
-            }
+        // Exception is caught by the parent method & an empty optional is returned...
+        if (response.statusCode() != 200) {
+            throw new IOException("Illegal response code: " + response.statusCode());
         }
-        return tempFile;
+
+        // Write to the provided output stream
+        try (final InputStream body = response.body()) {
+            body.transferTo(fileOutputStream);
+        }
     }
 
-
-    private String getVoice() {
+    /**
+     * @param voice the voice to get the id from
+     * @return the corresponding voice id for a given {@link StreamElementsConfiguration.StreamElementsVoice} or a randomly selected voice.
+     */
+    private String getVoice(final StreamElementsConfiguration.StreamElementsVoice voice) {
         return voice == StreamElementsConfiguration.StreamElementsVoice.RANDOM ?
                 ArrayUtil.getRandomEnumValue(StreamElementsConfiguration.StreamElementsVoice.values()).voiceName()
                 : voice.voiceName();
