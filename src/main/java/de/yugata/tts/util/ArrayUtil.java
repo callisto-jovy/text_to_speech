d55f76e594fa99180044f1c389e58930ea88b31a
callisto-jovy <48448458+callisto-jovy@users.noreply.github.com>
2024-02-13 16:25:21 +0100

    Documentation for ArrayUtil.java

--- src/main/java/de/yugata/tts/util/ArrayUtil.java
+++ src/main/java/de/yugata/tts/util/ArrayUtil.java
@@ -6,13 +6,15 @@ public class ArrayUtil {
     /**
      * Private constructor. Prevent instantiation.
      */
-    private ArrayUtil() {}
+    private ArrayUtil() {
+    }
 
     /**
+     * Grabs a random value using {@link ThreadLocalRandom} from the given enum value array.
      *
-     * @param values
-     * @return
-     * @param <E>
+     * @param values the enum values as an array to grab a random value.
+     * @param <E>    extends a Enum
+     * @return random value from the supplied array.
      */
     public static <E extends Enum<E>> E getRandomEnumValue(final E[] values) {
         return values[ThreadLocalRandom.current().nextInt(values.length)];
